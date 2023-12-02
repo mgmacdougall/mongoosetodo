e6e0bbede006dcc8478e87f7eea7811825734629
Michael MacDougall <mgmacdougall@outlook.com>
2023-12-02 13:15:40 -0500

    Add of the admin router

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.json({name: 'about-page'});
+  res.json({name: 'admin'});
 });
 
 module.exports = router;
